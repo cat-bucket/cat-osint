5a81acd2a6ab7a065a4a0c2a185eebb0420ef414
cat-bucket <gitbit843@gmail.com>
2024-08-31 14:21:54 +0800

    Update main.py
    Signed-off-by: cat-bucket <gitbit843@gmail.com>

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-qwe = [str(i) for i in range(1, 257)]
-
-
-def fetch_url(ip_suffix):
-    url = f"http://121.196.237.{ip_suffix}"
+def fetch_url(base_url, ip_suffix):
+    url = f"{base_url}.{ip_suffix}"
     try:
         response = requests.get(url)
         if response.status_code == 200:
             print(url)
     except requests.exceptions.RequestException:
-        
         pass
-      
+
 def main():
-    with ThreadPoolExecutor(max_workers=64) as executor:  
-        
-        futures = {executor.submit(fetch_url, ip_suffix): ip_suffix for ip_suffix in qwe}
+    base_url = input("输入前9位ip号段(如 http://121.196.237): ")  # 获取用户输入的基本URL
+    qwe = [str(i) for i in range(1, 257)]
+    
+    with ThreadPoolExecutor(max_workers=64) as executor:
+        futures = {executor.submit(fetch_url, base_url, ip_suffix): ip_suffix for ip_suffix in qwe}
 
         for future in as_completed(futures):
-  
             pass
 
 if __name__ == "__main__":
